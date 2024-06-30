10c2208bb57081368da290d140649c54b40b4b67
Alexander Walden <arwalden4@outlook.com>
2024-06-30 12:52:56 -0500

    pushing changes

--- EmployeeSQL/employee_schema.sql
+++ EmployeeSQL/employee_schema.sql
@@ -50,7 +50,7 @@ CREATE TABLE dept_manager (
 CREATE TABLE salaries (
     emp_no INT NOT NULL,
     salary INT NOT NULL,
-    FOREIGN KEY (emp_no) REFERENCES employees (emp_no)
+    FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
     PRIMARY KEY (emp_no) 
 );
 
